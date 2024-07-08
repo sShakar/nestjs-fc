2e814c4faffd93599628790ba25226a3d94c1798
Shakar Kosar <shakarkosar01@gmail.com>
2024-07-08 15:22:31 +0300

    added get and post requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  @Get()
+  findAll() {
+    return 'This action return all the coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Return flavors ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+}
