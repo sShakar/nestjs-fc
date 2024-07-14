1f1fda05d9bad6ccf107f8c0f499c1cceb949039
Shakar Kosar <shakarkosar01@gmail.com>
2024-07-14 10:36:50 +0300

    feat: added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
