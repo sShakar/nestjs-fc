3cda8dc9fca25eafdd3bf15099c48721c7ad6bcb
Shakar Kosar <shakarkosar01@gmail.com>
2024-08-14 15:46:26 +0300

    feat: adding non class based provider tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,11 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from '@/coffees/entities/coffee.entity';
 import { Flavor } from '@/coffees/entities/flavor.entity';
 import { Event } from '@/events/entities/event.entity';
+import { COFFEE_BRANDS } from '@/coffees/coffees.constants';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
