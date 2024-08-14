e93e0ba7b1c11c3640fdd0ba8b686783b3a47749
Shakar Kosar <shakarkosar01@gmail.com>
2024-08-14 16:05:35 +0300

    feat: adding class based provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,11 +7,22 @@ import { Flavor } from '@/coffees/entities/flavor.entity';
 import { Event } from '@/events/entities/event.entity';
 import { COFFEE_BRANDS } from '@/coffees/coffees.constants';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
   ],
   exports: [CoffeesService],
