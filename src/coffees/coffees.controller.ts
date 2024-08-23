0147fe45646171fab0b9e168ae25df725f3c98e5
sShakar <shakarkosar01@gmail.com>
2024-08-23 22:45:17 +0300

    feat: added custom integer validation pipe

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,6 +13,7 @@ import { CreateCoffeeDto } from '@/coffees/dto/create-coffee.dto';
 import { UpdateCoffeeDto } from '@/coffees/dto/update-coffee.dto';
 import { PaginationQueryDto } from '@/common/dto/pagination-query.dto';
 import { Public } from '@/common/decorators/public.decorator';
+import { ParseIntPipe } from '@/common/pipes/parse-int.pipe';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -20,14 +21,14 @@ export class CoffeesController {
 
   @Public()
   @Get()
-  async findAll(@Query() paginationQuery: PaginationQueryDto) {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.coffeesService.findAll(paginationQuery);
   }
 
   @Public()
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: string) {
+    console.log(id);
     return this.coffeesService.findOne(id);
   }
 
