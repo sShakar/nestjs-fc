b303e12ca107998049e90571649f9322d464be7d
sShakar <shakarkosar01@gmail.com>
2024-08-23 22:57:28 +0300

    feat: added logging middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from '@/common/guards/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingMiddleware } from '@/common/middlewares/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
   providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
