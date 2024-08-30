a413bb1e5fd0f977acf212e42885b5f04d97660d
sShakar <shakarkosar01@gmail.com>
2024-08-30 13:25:35 +0300

    feat: added swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'tsconfig-paths/register';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from '@/app.module';
 import { HttpExceptionFilter } from '@/common/filters/http-exception.filter';
 import { WrapResponseInterceptor } from '@/common/interceptors/wrap-response.interceptor';
@@ -24,6 +25,14 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Coffee')
+    .setDescription('Coffee APIs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
